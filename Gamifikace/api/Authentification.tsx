df7d428ee9dfd265dbc078ea75cf99e682c8ecfc
Jakub007d <dr.kubko@gmail.com>
2025-02-22 19:26:10 +0100

    Initial commit

---
+++ Gamifikace/api/Authentification.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage
+import fetchUserID from "./Downloaders/fetchUserID";
+interface User {
+  username: string;
+  password: string;
+}
+
+interface TokenResponse {
+  access: string; // Change based on your API response
+  refresh: string; // Change based on your API response
+}
+export async function Authentification(user: User): Promise<boolean> {
+  console.log("Logovanie");
+  try {
+    const response = await axios.post("https://gamifikace.lol/token/", user, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: false,
+    });
+
+    // Clear stored tokens
+    {
+      /*
+        await AsyncStorage.removeItem("access_token");
+        await AsyncStorage.removeItem("refresh_token");
+        await AsyncStorage.removeItem("user_id");
+      */
+    }
+
+    // Store tokens using AsyncStorage
+    await AsyncStorage.removeItem("access_token");
+    await AsyncStorage.setItem("access_token", response.data.access);
+    console.log(response.data.access);
+    await AsyncStorage.setItem("refresh_token", response.data.refresh);
+    const userID = await fetchUserID(response.data.access);
+    await AsyncStorage.setItem("user_id", userID);
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${response.data.access}`;
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
---
+++ Gamifikace/app/index.tsx
@@ -0,0 +1,107 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Center } from "@/components/ui/center";
+import { Icon } from "@/components/ui/icon";
+import { Pressable } from "@/components/ui/pressable";
+import { Spinner } from "@/components/ui/spinner";
+import { VStack } from "@/components/ui/vstack";
+import { useToast, Toast } from "@/components/ui/toast";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage
+import { Authentification } from "../api/Authentification";
+import { router } from "expo-router";
+import { Input, InputField, InputSlot } from "@/components/ui/input";
+interface User {
+  username: string;
+  password: string;
+}
+
+interface TokenResponse {
+  access: string; // Change based on your API response
+  refresh: string; // Change based on your API response
+}
+
+const login = () => {
+  const toast = useToast();
+  const [show, setShow] = React.useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [username, setUsername] = useState("admin");
+  const [password, setPassword] = useState("Heslo123123!");
+  const [isTokenSet, setIsTokenSet] = useState(false); // Track if token is set
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem("access_token");
+        if (token) {
+          setIsTokenSet(true);
+        }
+      } catch (error) {
+        console.error("Error fetching token:", error);
+      }
+    };
+    checkToken();
+  }, []);
+  async function handleLogin(user: User) {
+    setLoading(true);
+    try {
+      const result = await Authentification(user);
+
+      if (result) {
+        router.push({
+          pathname: "/courses/course_selector/[user_id]",
+          params: { user_id: username },
+        });
+      } else {
+        setLoading(false);
+        toast.show({ description: "Zle zadané údaje" });
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.show({ description: "Chyba prihlásenia" });
+    }
+  }
+  return (
+    <Center className="flex-1 bg-white">
+      {" "}
+      {/* Centers content vertically and horizontally */}
+      <VStack className="space-4 w-full items-center">
+        <Input className="variant-outline size-md">
+          <InputSlot className="pl-3">
+            <Icon as={MaterialIcons} size="md" className="text-muted-500" />
+          </InputSlot>
+          <InputField
+            placeholder="Meno"
+            value={username}
+            onChangeText={setUsername}
+          />
+        </Input>
+        {/* Password Input */}
+        <Input className="variant-outline size-md">
+          <InputField
+            placeholder="Heslo"
+            type={show ? "text" : "password"}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <InputSlot className="pr-3">
+            <Pressable onPress={() => setShow(!show)}>
+              <Icon as={MaterialIcons} size="md" className="text-muted-500" />
+            </Pressable>
+          </InputSlot>
+        </Input>
+        {isLoading ? (
+          <Spinner color="blue.500" /> // Adjust the size and color as needed
+        ) : (
+          <Button
+            onPress={() =>
+              handleLogin({ username: username, password: password })
+            }
+          >
+            <ButtonText>Prihlásiť sa</ButtonText>
+          </Button>
+        )}
+      </VStack>
+    </Center>
+  );
+};
+export default login;
